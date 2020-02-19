96c0565c05d99af77179cda872ffb8c0e6221665
Ann Gould <gould.ann8@gmail.com>
2020-02-18 21:40:40 -0600

    final modifications

--- tests/script_tests.py
+++ tests/script_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from FacialRecog import *
 from RecogScript import process_video_to_encode
@@ -11,11 +12,11 @@ class ScriptTest(unittest.TestCase):
     # all tests must start with "test"
     def test_headless_encoding(self):
         foo = get_video_from_file("test_ann.mov")
-        employee_dir = "../images/employees"
-        encodings = process_video_to_encode(foo, employee_dir, "../images/temp.jpeg")
+        # employee_dir = "images/employees"
+        # encodings = process_video_to_encode(foo, employee_dir, "../images/temp.jpeg")
         # asserts true that the video was able to find Ann :)
-        self.assertTrue(encodings[1])
-
+        # self.assertTrue(encodings[1])
+        self.assertEquals(os.getcwd(), "foo")
 
 
 if __name__ == '__main__':
