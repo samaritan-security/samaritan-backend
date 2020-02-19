60f5e21cc8c987493cf9f7f4cf3ce1e33dd17f68
Ann Gould <gould.ann8@gmail.com>
2020-02-18 21:49:58 -0600

    should work now
    final modifications
    
    final modifications
    
    final modifications
    
    removed tests, changed to work locally
    
    removed tests, changed to work locally??? where the fuck did my last commit go lol

--- tests/script_tests.py
+++ tests/script_tests.py
@@ -1,5 +1,4 @@
 import unittest
-import os
 
 from FacialRecog import *
 from RecogScript import process_video_to_encode
@@ -12,11 +11,11 @@ class ScriptTest(unittest.TestCase):
     # all tests must start with "test"
     def test_headless_encoding(self):
         foo = get_video_from_file("test_ann.mov")
-        # employee_dir = "images/employees"
-        # encodings = process_video_to_encode(foo, employee_dir, "../images/temp.jpeg")
+        # employee_dir = "/home/runner/work/samaritan-backend/samaritan-backend/images/employees"
+        employee_dir = "../images/employees"
+        encodings = process_video_to_encode(foo, employee_dir, "temp.jpeg")
         # asserts true that the video was able to find Ann :)
-        # self.assertTrue(encodings[1])
-        self.assertEquals(os.getcwd(), "foo")
+        self.assertTrue(encodings[1])
 
 
 if __name__ == '__main__':
