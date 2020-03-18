3706f05de5d3ec6f7f3dc2db1d9831abd8c6b611
rgoluch <rgoluch@iastate.edu>
2020-03-18 13:53:57 -0500

    working on unittests for multiple cameras

---
+++ tests/multi_camera_tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from RecogScript import *
+from FacialRecog import get_video_from_file
+
+class MultiCameraTests(unittest.TestCase):
+
+    """
+    Set up for running tests on multiple camera feeds. Multiple camera feeds
+    represented by multiple video recordings being used.
+    """
+    def setup(self):
+        # TODO
+
+    def te
+
+if __name__ == '__main__':
+    unittest.main()
