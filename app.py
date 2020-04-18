0981f3a1116f47fa9ef1526bed78274ca9881a21
rgoluch <rgoluch@iastate.edu>
2020-04-18 15:51:10 -0500

    fixing alerts

--- app.py
+++ app.py
@@ -430,8 +430,8 @@ def add_new_alert(ref_id: str, camera_id: str, email: str):
         "created_at": time
     }
     if len(email) > 0:
-        image = get_person_by_id(alert.ref_id).image
-        camera = get_camera_by_id(alert.camera_id)
+        image = get_person_by_id(alert['ref_id']).image
+        camera = get_camera_by_id(alert['camera_id'])
         send_alert_email(alert, image, camera, email)
     result = db.alerts.insert_one(alert)
     return result
