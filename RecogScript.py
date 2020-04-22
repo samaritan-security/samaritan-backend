a4c07e0b65eb9b7c046090a09f940d99d8c1d3ca
Ann Gould <gould.ann8@gmail.com>
2020-04-21 23:53:18 -0500

    added TimeoutQueue functionality. Figuring out how to test

--- RecogScript.py
+++ RecogScript.py
@@ -10,22 +10,32 @@ Author(s): Devin Uner, Ryan Goluch, Ann Gould
 
 from FacialRecog import all_cameras, get_all_people_information, get_frame_from_camera, compare_frame_and_encodings, process_comparisons
 import imagezmq
-'''
-Main script function
-'''
+from timeoutqueue import TimeoutQueue
 
 
 def main():
+    """
+    Main script function
+    """
     cameras = all_cameras()
     all_ids, all_encodings = get_all_people_information()
     image_hub = imagezmq.ImageHub()
+    timeout_queue = TimeoutQueue(4)
 
     while True:
         for camera in cameras:
             frame = get_frame_from_camera(image_hub, camera)
             comparison_results = compare_frame_and_encodings(
                 frame, all_encodings)
-            process_comparisons(comparison_results, all_ids, camera, frame)
+            if comparison_results is not None:
+                if len(timeout_queue.get()) is not 0:
+                    # print("hitting timeoutqueue")
+                    if comparison_results not in timeout_queue.get():
+                        timeout_queue.append(comparison_results)
+                        process_comparisons(comparison_results, all_ids, camera, frame)
+                else:
+                    # print("now being added")
+                    timeout_queue.append(comparison_results)
 
 
 if __name__ == "__main__":
